090cc7d037054bbed01a95e3a1728007748dcf33
Eric Bortoleto <eric.bortoleto@totvs.com.br>
2026-09-25 10:07:58 -0300

    refactor(ui): change the MainActivity.kt composable component to CatalogScreen.kt

--- app/src/main/java/com/example/ericac/MainActivity.kt
+++ app/src/main/java/com/example/ericac/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.ericac.ui.screens.CatalogProducts
+import com.example.ericac.ui.screens.CatalogScreen
 import com.example.ericac.ui.theme.EricACTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EricACTheme {
-                CatalogProducts()
+                CatalogScreen()
             }
         }
     }
--- app/src/main/java/com/example/ericac/ui/screens/CheckoutBar.kt
+++ app/src/main/java/com/example/ericac/ui/screens/CheckoutBar.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
@@ -28,6 +29,7 @@ fun CheckoutBar() {
             .background(Color.Black)
             .padding(16.dp)
             .padding(bottom = 4.dp)
+            .navigationBarsPadding()
             .fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
